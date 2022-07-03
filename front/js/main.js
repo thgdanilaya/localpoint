b5275170dc3dd7dfc94573a879a9d46a07ef25dd
kiviyi <90795245+kiviyi@users.noreply.github.com>
2022-07-04 02:09:15 +0300

    bug fix 03.07.22

--- front/js/main.js
+++ front/js/main.js
@@ -1,7 +1,7 @@
 //Показ пароля при регистрации и вводе
-function show_hide_password(target){
+function show_hide_password(target) {
     let pass = document.getElementsByClassName('pass');
-    for(let input of pass){
+    for (let input of pass) {
         if (input.getAttribute('type') == 'password') {
             target.classList.add('view');
             input.setAttribute('type', 'text');
@@ -10,18 +10,18 @@ function show_hide_password(target){
             input.setAttribute('type', 'password');
         }
     }
-	
+
 }
 //Сравнение паролей
 
-function srav(inppass, inprep){
+function srav(inppass, inprep) {
     let button = document.getElementById('sing_up');
     var pass = document.getElementById(inppass);
     var repeat_pass = document.getElementById(inprep);
-    if (pass.value === repeat_pass.value && pass.value != ""){
+    if (pass.value === repeat_pass.value && pass.value != "") {
         button.classList.remove("disabled");
     }
-    else{
+    else {
         button.classList.add("disabled");
     }
-  }
+}
