6286501bb0423a973a8c9c0107cd972256fd63c1
thgdanilaya <frederigo234@gmail.com>
2022-07-07 20:53:43 +0300

    кривой переход со странице хоум на регистрацию

---
+++ localpoint/registration/static/registration/js/main.js
@@ -0,0 +1,27 @@
+//Показ пароля при регистрации и вводе
+function show_hide_password(target) {
+    let pass = document.getElementsByClassName('pass');
+    for (let input of pass) {
+        if (input.getAttribute('type') == 'password') {
+            target.classList.add('view');
+            input.setAttribute('type', 'text');
+        } else {
+            target.classList.remove('view');
+            input.setAttribute('type', 'password');
+        }
+    }
+
+}
+//Сравнение паролей
+
+function srav(inppass, inprep) {
+    let button = document.getElementById('sing_up');
+    var pass = document.getElementById(inppass);
+    var repeat_pass = document.getElementById(inprep);
+    if (pass.value === repeat_pass.value && pass.value != "") {
+        button.classList.remove("disabled");
+    }
+    else {
+        button.classList.add("disabled");
+    }
+}
