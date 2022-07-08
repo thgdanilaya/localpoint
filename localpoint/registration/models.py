3131826d5d17430f200ff7fe55e8d7d158506b57
thgdanilaya <frederigo234@gmail.com>
2022-07-08 21:10:33 +0300

    не могу заинсертить в базу данных

--- localpoint/registration/models.py
+++ localpoint/registration/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
+class Users(models.Model):
     username = models.CharField(max_length=35)
-    email = models.EmailField
-    user_photo = models.ImageField
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
     is_registered = models.BooleanField(default=True)
