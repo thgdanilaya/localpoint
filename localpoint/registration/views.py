de005324caa6dd3ed2e071cd5c8fba8791b700b4
thgdanilaya <frederigo234@gmail.com>
2022-07-09 00:10:29 +0300

    не работает метод пост

--- localpoint/registration/views.py
+++ localpoint/registration/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpResponse
+from registration.models import Users
 
 # Create your views here.
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 from registration.forms import RegisterUserForm
 
@@ -16,7 +17,6 @@ class Registration(CreateView):
     success_url = reverse_lazy('home')
 
 
-
 def home(request):
     return render(request, "registration/home.html")
 
@@ -25,12 +25,14 @@ def reg_success(request):
     if request.method == 'POST':
         form_name = RegisterUserForm(request.POST)
         if form_name.is_valid():
-            from registration.models import Users
             username = form_name.cleaned_data.get("Inputusername")
             email = form_name.cleaned_data.get("Inputemail")
             password = form_name.cleaned_data.get("InputPassword")
+            print(username, email, password)
             b = Users(username=username, email=email, password=password)
             b.save()
+            return render(request, 'registration/home.html')
+    return HttpResponseNotFound("hello")
 
 
 class LoginUser(LoginView):
