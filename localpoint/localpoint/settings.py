d88c3f8b2404e6e61bc85767b40730f7de6a1819
Danila <82927712+thgdanilaya@users.noreply.github.com>
2022-07-11 20:30:10 +0300

    Update settings.py
    исправил косяк

--- localpoint/localpoint/settings.py
+++ localpoint/localpoint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-91j#$ykh%*t+)o_uhuah&ws!7y3*m3mtu0r)k8#ejq1zgznfjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['5.188.143.210']
 
 JWT_AUTH = {
     'JWT_VERIFY': True,
@@ -79,16 +79,15 @@ WSGI_APPLICATION = 'localpoint.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'users',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+}
 }
 
 
